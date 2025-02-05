e876e758e1123b4f5917d342e3463459630362cc
onebalddeveloper <andyiwebeluajarret@gmail.com>
2025-02-05 15:56:26 +0000

    This is a simple calculator

---
+++ example.js
@@ -0,0 +1,12 @@
+//1
+
+let temperature = 1;
+if (temperature < 20){
+    console.log('The temperature is low')
+}
+else if(temperature < 2){
+    console.log("The temperature is too low, I'm gonna freeze")
+}
+else{
+    console.log('The temperature is high')
+}
